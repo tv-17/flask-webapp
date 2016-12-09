ef15d2b63d68799c4c539be35f6b8d622b16c21a
Thivan <thivan.visvanathan@cloudreach.com>
2016-12-09 08:53:05 +0000

    Major::Complete infrastructure + app

--- README.md
+++ README.md
@@ -8,3 +8,10 @@
 ### Deploying the infrastructure follows the principles of Infrastructure as Code. The entire stack is written in troposphere (a python wrapper around native cloudformation). Using troposphere allows us to take advantage of the logic presented by a high level programming language (i.e. adding conditional logic and the ability to integrate with other application code).
 
 make create_stack
+
+- deploys asg
+- latest copy of source code is pulled down by instances from s3
+- userdata provisions instance by setting up apache, mod_wsgi
+to serve webapp
+- autoscaling actions
+- adds cname alias to route53 record
--- cloudformation/troposphere/webapp.py
+++ cloudformation/troposphere/webapp.py
@@ -1,11 +1,11 @@
 from troposphere import Base64, Join
 from troposphere import Parameter, Ref, Template
-from troposphere.autoscaling import AutoScalingGroup
-from troposphere.autoscaling import LaunchConfiguration
+from troposphere.autoscaling import AutoScalingGroup, LaunchConfiguration, ScalingPolicy
 from troposphere.elasticloadbalancing import LoadBalancer
 from troposphere.policies import AutoScalingReplacingUpdate, AutoScalingRollingUpdate, UpdatePolicy
-from troposphere.ec2 import SecurityGroup, SecurityGroupRule, EBSBlockDevice, BlockDeviceMapping
+from troposphere.ec2 import SecurityGroup, SecurityGroupRule
 from troposphere.elasticloadbalancing import ConnectionDrainingPolicy, HealthCheck, Listener
+from troposphere.cloudwatch import Alarm, MetricDimension
 
 
 class WebApp(object):
@@ -134,12 +134,22 @@ class WebApp(object):
             "LaunchConfiguration",
             UserData=Base64(Join('', [
                 "#!/bin/bash\n",
-                "sudo yum install httpd -y\n",
+                "sudo yum install httpd mod_wsgi -y\n",
+                "sudo pip install flask\n",
                 "sudo chkconfig httpd on\n",
                 "sudo service httpd start\n",
-                "sudo touch /var/www/html/index.html\n",
                 "sudo service httpd restart\n",
-                "aws s3 cp s3://thivan-sample-data/webapp-", Ref(webapp_zip), " .\n",
+                "sudo aws s3 cp s3://thivan-sample-data/", Ref(webapp_zip), " .\n",
+                "sudo unzip ", Ref(webapp_zip), "\n",
+                "sudo mv /home/ec2-user/app /var/www/html/\n",
+                "sudo mv app /var/www/html/\n",
+                "sudo mv /var/www/html/app/server_config/wsgi.conf /etc/httpd/conf.d/\n",
+                "sudo groupadd group1\n",
+                "sudo useradd user1 -g group1\n",
+                "sudo usermod -a -G group1 apache\n",
+                "sudo chown -vR :group1 /var/www/\n",
+                "sudo chmod -vR g+w /var/www/\n",
+                "sudo service httpd restart\n",
                 "cfn-signal -e 0",
                 "    --resource AutoscalingGroup",
                 "    --stack ", Ref("AWS::StackName"),
@@ -153,7 +163,7 @@ class WebApp(object):
             InstanceType="t2.micro",
         ))
 
-        self.t.add_resource(AutoScalingGroup(
+        asg = self.t.add_resource(AutoScalingGroup(
             "AutoscalingGroup",
             DesiredCapacity=2,
             LaunchConfigurationName=Ref(launch_config),
@@ -175,6 +185,32 @@ class WebApp(object):
             )
         ))
 
+        scaling_policy = self.t.add_resource(ScalingPolicy(
+            "ScalingPolicy",
+            AdjustmentType="ChangeInCapacity",
+            AutoScalingGroupName=Ref(asg),
+            Cooldown="120",
+            ScalingAdjustment="1"
+        ))
+
+        self.t.add_resource(Alarm(
+            "CPUAlarm",
+            EvaluationPeriods="1",
+            Statistic="Maximum",
+            Threshold="50",
+            AlarmDescription="Alarm if CPU too high or metric disappears indicating instance is down",
+            Period="60",
+            AlarmActions=[Ref(scaling_policy)],
+            Namespace="AWS/EC2",
+            Dimensions=[
+                MetricDimension(
+                    Name="AutoScalingGroupName",
+                    Value=Ref(asg)
+                ),
+            ],
+            ComparisonOperator="GreaterThanThreshold",
+            MetricName="CPUUtilization"
+        ))
         return launch_config
 
 if __name__ == '__main__':
